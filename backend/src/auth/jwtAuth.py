81a89f82c6b58d1c682776a53ead7667ec92abd5
YeisonGonz <yeisonrascado@gmail.com>
2025-03-02 12:14:42 +0100

    secu(env): Upload env system

--- backend/src/auth/jwtAuth.py
+++ backend/src/auth/jwtAuth.py
@@ -3,10 +3,13 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-SECRET_KEY = "tu_clave_secreta"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
--- backend/src/rcon/rconClient.py
+++ backend/src/rcon/rconClient.py
@@ -1,8 +1,15 @@
 from mcrcon import MCRcon
 from loguru import logger
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+HOST = os.getenv('MINECRAFT_RCON_HOST')
+PASSWORD = os.getenv('MINECRAFT_RCON_PASSWORD')
+PORT = int(os.getenv('MINECRAFT_RCON_PORT'))
 
 def test_rcon(command):
-    with MCRcon("192.168.1.32", "custompassword", port=25575) as mcr:
+    with MCRcon(HOST, PASSWORD, port=PORT) as mcr:
         resp = mcr.command(command)
         logger.success(f"Command send: {command}")
         logger.success(f"Command response: {resp}")
